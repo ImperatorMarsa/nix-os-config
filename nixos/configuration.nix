443b6bfa99cdb1a5f003efaa6986a55ae13b4453
Timofey Sitnikov <tima.sitnikov@mail.ru>
2023-07-15 20:38:41 +0300

    enable fish as defualt user shel

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,11 +101,13 @@
     xkbVariant = "";
   };
 
+  programs.fish.enable = true;
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users.timofey = {
     # TODO: You can set an initial password for your user.
     # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
     # Be sure to change it (using passwd) after rebooting!
+    shell = pkgs.fish;
     isNormalUser = true;
     description = "Timofey";
     extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
