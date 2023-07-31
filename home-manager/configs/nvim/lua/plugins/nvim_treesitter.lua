7820d291bd21c1911bf0da94d821529ec17fd817
Sitnikov-TV <86051413+Sitnikov-TV@users.noreply.github.com>
2023-07-31 18:30:07 +0300

    Настроил tree-sitter

---
+++ home-manager/configs/nvim/lua/plugins/nvim_treesitter.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "css",
+                "html",
+                "javascript",
+                "jsdoc",
+                "json",
+                "lua",
+                "luadoc",
+                "luap",
+                "markdown",
+                "markdown_inline",
+                "php",
+                "query",
+                "regex",
+                "scss",
+                "sql",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "vue",
+            },
+        },
+    },
+}
