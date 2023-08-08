aeee0726aedb06900dccdf32569f0527a40e86c6
Timofey Sitnikov <tima.sitnikov@mail.ru>
2023-08-08 23:28:29 +0300

    setup buty UI

--- home-manager/configs/nvim/lua/options.lua
+++ home-manager/configs/nvim/lua/options.lua
@@ -55,3 +55,7 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+-- disable netrt
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
--- home-manager/configs/nvim/lua/pluginList.lua
+++ home-manager/configs/nvim/lua/pluginList.lua
@@ -4,7 +4,7 @@ return {
     --
     -- Color Theme
     {
-        "dracula/vim",
+        "Mofiqul/dracula.nvim",
         priority = 1000,
         config = function()
             vim.cmd("colorscheme dracula")
@@ -21,7 +21,7 @@ return {
         config = function()
             require("lualine").setup({
                 options = {
-                    theme = 'dracula',
+                    theme = 'dracula-nvim',
                     icons_enabled = true,
                     section_separators = '',
                     component_separators = '⸽',
@@ -29,8 +29,59 @@ return {
             })
         end,
     },
+
+    -- Indent Hightlite
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            vim.opt.list = true
+            vim.opt.listchars:append "space:•"
+
+            vim.opt.termguicolors = true
+            vim.cmd [[highlight IndentBlanklineIndent1 guibg=#2d2f3d gui=nocombine]]
+            vim.cmd [[highlight IndentBlanklineIndent2 guibg=#282a36 gui=nocombine]]
+            vim.cmd [[highlight IndentBlanklineIndent3 guibg=#22242e gui=nocombine]]
+
+            require("indent_blankline").setup ({
+                space_char_blankline = " ",
+                show_current_context = true,
+                show_current_context_start = true,
+
+                char = "",
+                char_highlight_list = {
+                    "IndentBlanklineIndent1",
+                    "IndentBlanklineIndent2",
+                    "IndentBlanklineIndent3",
+                },
+                space_char_highlight_list = {
+                    "IndentBlanklineIndent1",
+                    "IndentBlanklineIndent2",
+                    "IndentBlanklineIndent3",
+                },
+                show_trailing_blankline_indent = false,
+            })
+        end,
+    },
     -- -- --
     --
+    --
+
+    --
+    -- Utils
+    --
+    -- Nvim tree
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
+    },
+    -- -- --
     --
     --
 }
