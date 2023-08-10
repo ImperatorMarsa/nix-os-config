ad9321ef03ce968635da670b1119c5f623ce5bf4
Sitnikov-TV <86051413+Sitnikov-TV@users.noreply.github.com>
2023-08-10 09:43:26 +0300

    Дополнил комбинации клавиш
    удобная навигация между окнами

--- home-manager/configs/nvim/lua/keymap.lua
+++ home-manager/configs/nvim/lua/keymap.lua
@@ -2,6 +2,28 @@ function KeyMap(mode, lhs, rhs, opts)
     vim.keymap.set(mode, lhs, rhs, opts)
 end
 
+-- general maping
+KeyMap({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- better up/down
+KeyMap({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+KeyMap({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+KeyMap("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+KeyMap("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+KeyMap("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+KeyMap("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+
+-- Move Lines
+KeyMap("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+KeyMap("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+KeyMap("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+KeyMap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+KeyMap("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+KeyMap("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+
 -- telescope general
 local builtin = require('telescope.builtin')
 KeyMap(
@@ -18,4 +40,13 @@ KeyMap('n', '<leader>fg', builtin.live_grep, {})
 KeyMap('n', '<leader>fb', builtin.buffers, {})
 KeyMap('n', '<leader>fh', builtin.help_tags, {})
 KeyMap('n', '<leader>fj', builtin.jumplist, {})
-KeyMap('n', '<leader>fS', builtin.spell_suggest, {})
+KeyMap(
+    'n',
+    '<leader>fS',
+    function()
+        builtin.spell_suggest(
+            require('telescope.themes').get_cursor()
+        )
+    end,
+    {}
+)
