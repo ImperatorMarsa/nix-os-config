64708708816f3f1a3583897d0a4dfe7d1325bb4b
Timofey Sitnikov <tima.sitnikov@mail.ru>
2023-09-19 18:45:08 +0000

    добавил nix-конфиг для андроида

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -24,9 +29,13 @@
     # nix-colors.url = "github:misterio77/nix-colors";
   };
 
-  outputs = { nixpkgs, home-manager, nur, nixos-hardware, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, nur, nixos-hardware, nix-on-droid, ... }@inputs: {
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#think-nix'
+    nixOnDroidConfigurations.nexus = nix-on-droid.lib.nixOnDroidConfiguration {
+      modules = [ ./nixos/droid/nix-on-droid.nix ];
+    };
+
     nixosConfigurations = {
       # FIXME replace with your hostname
       think-nix = nixpkgs.lib.nixosSystem {
